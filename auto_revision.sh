a9fb93c98f23752d77592516fb51bc8e65d03c2f
mohammadll <97779392+mohammadll@users.noreply.github.com>
2022-04-09 18:02:41 +0430

    Update auto_revision.sh

--- auto_revision.sh
+++ auto_revision.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-BASE_VERSION=$(cat /var/lib/jenkins/workspace/teller-base-keys_release-base/target/maven-archiver/pom.properties | grep version | cut -d "=" -f2)
 version=$(git describe --tags --abbrev=0)
 last_commit=$(git log -1 | tail -n 1 | awk '{$1=$1;print}')
 regex="([0-9]+)\\.([0-9]+)\\.([0-9]+)"
@@ -14,18 +13,17 @@ if [[ $last_commit == CHANGE:* ]]
 			minor=0
 			patch=0
 			major_change=$major.$minor.$patch
-			mvn clean deploy -Drevision=$major_change -DBASE_VERSION=$BASE_VERSION scm:tag
+			mvn clean deploy -Drevision=$major_change scm:tag
 elif [[ $last_commit == feat:* ]]
 	 then
 			minor=$(echo $minor + 1 | bc)
 			patch=0
 			minor_change=$major.$minor.$patch
-			mvn clean deploy -Drevision=$minor_change -DBASE_VERSION=$BASE_VERSION scm:tag
+			mvn clean deploy -Drevision=$minor_change scm:tag
 elif [[ $last_commit == fix:* ]]
 	 then
 			patch=$(echo $patch + 1 | bc)
 			patch_change=$major.$minor.$patch
-			mvn clean deploy -Drevision=$patch_change -DBASE_VERSION=$BASE_VERSION scm:tag
+			mvn clean deploy -Drevision=$patch_change scm:tag
 fi
 fi
-echo "teller base key version: $BASE_VERSION"
